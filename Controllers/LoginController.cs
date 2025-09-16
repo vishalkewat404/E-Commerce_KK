4aa8107ffe27229965e1b5204093388662b2fc0f
Vishal Kewat <VishalKevat343@gmai.com>
2025-09-16 12:00:28 +0530

    Signed-off-by: Vishal Kewat <VishalKevat343@gmai.com>

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult LoginPage()
+		{
+			return View();
+		}
+	}
+}
